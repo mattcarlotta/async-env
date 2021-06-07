19ccc58cc4f8364fc9d3072f09f5b9d7f1e5d394
Matt <carlotta.matt@gmail.com>
2021-06-07 14:02:13 -0700

    Add extending envs thru comments

--- example.js
+++ example.js
@@ -1,5 +1,10 @@
-const { config } = require("@noshot/env");
+const { readFileSync } = require("fs");
+const { config, parse } = require("@noshot/env");
 
+// dynamic properties
 const { extracted } = config({ paths: ".env.dynamicdata" });
-
 console.log("🚀 extracted: ", extracted);
+
+// extended envs from magic comments
+const result = parse(readFileSync(".env.extends"));
+console.log("🚀 result", result);
